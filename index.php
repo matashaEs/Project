d9c49020de1b4bdf99e12f0f83f1c1aaaa911cf4
Adam Basa <>
2022-07-08 14:13:50 +0200

    jest ok

--- index.php
+++ index.php
@@ -6,10 +6,6 @@ get_header();
 
 <?php
 echo esc_html( get_template_part( 'template-parts/pagination', 'infinite-scroll' ) );
-
-$moja_zmienna = sanitize_text_field( $_POST['zmienna'] );
-
-echo $moja_zmienna;
 ?>
 
 <?php
