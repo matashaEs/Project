479c4943d1b2ce64b549865507f3cbb72e174b5b
Mariia Maliuk <>
2022-07-08 17:08:17 +0200

    cleared index

--- index.php
+++ index.php
@@ -8,14 +8,5 @@ get_header();
 echo esc_html( get_template_part( 'template-parts/pagination', 'infinite-scroll' ) );
 ?>
 
-<button>Button usual</button>
-<button class="button--footer">Button footer</button>
-<button class="button--white">Button white</button>
-<button class="button--white button--white-blue">Button white blue</button>
-<button class="button--blue-border">Button blue border</button>
-<button class="button--orange">Button orange</button>
-<button class="button--dark-pink">Button dark pink</button>
-<button class="button--light-pink">Button light pink</button>
-<button class="button--wms">Button WMS</button>
 <?php
 get_footer();
