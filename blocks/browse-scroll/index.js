8f59c87a912a5fde51edfc4a34d42b0ce1f6afe6
Adam Basa <>
2022-07-11 19:33:43 +0200

    progress

--- blocks/browse-scroll/index.js
+++ blocks/browse-scroll/index.js
@@ -1,1 +1,20 @@
 import './browse-scroll.scss';
+import $ from 'jquery';
+
+class BrowseScroll {
+	constructor() {
+		// this.slider();
+	}
+
+	slider() {
+		$( '.browse-scroll__items-list' ).slick({
+			infinite: true,
+			slidesToShow: 2,
+			slidesToScroll: 1,
+			arrows: false,
+			dots: true,
+		});
+	}
+}
+
+new BrowseScroll();
