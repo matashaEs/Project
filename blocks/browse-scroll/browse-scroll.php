ab5899a19f68b69fe6332b3cf48efa7c59941779
Adam Basa <>
2022-07-12 14:23:07 +0200

    browse init

---
+++ blocks/browse-scroll/browse-scroll.php
@@ -0,0 +1,43 @@
+<?php
+$browse_by = '';
+
+
+if ( ! empty( $block['id'] ) ) {
+	extract( get_fields( $block['id'] ) );
+} else {
+	extract( $block['data'] );
+}
+
+if ( 'industry' === $browse_by ) {
+	$list_title = __( 'Browse by Industry', 'nuplo' );
+	$items      = apply_filters( 'cai_get_industries', null );
+} else {
+	$list_title = __( 'Browse by Product', 'nuplo' );
+	$items      = apply_filters( 'cai_get_products', null );
+}
+?>
+
+<section class="container-fluid">
+	<div class="container browse-scroll">
+		<div class="row browse-scroll__row">
+			<div class="browse-scroll__description">
+				<div class="browse-scroll__description-bg"></div>
+				<a class="browse-scroll__description-link h2" href="#">
+					<?= esc_html( $list_title ); ?>
+				</a>
+			</div>
+
+			<div class="browse-scroll__items">
+				<div class="browse-scroll__items-list">
+					<?php foreach ( $items as $item ) : ?>
+						<div class="browse-scroll__item">
+							<div class="browse-scroll__item-bg"
+								 style="background-image: url('<?= esc_html( $item['background'] ); ?>');"></div>
+							<a class="button button--white browse-scroll__button"><?= esc_html( $item['name'] ); ?></a>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ blocks/browse-scroll/index.js
@@ -0,0 +1,20 @@
+import './browse-scroll.scss';
+import $ from 'jquery';
+
+class BrowseScroll {
+	constructor() {
+		this.slider();
+	}
+
+	slider() {
+		$( '.browse-scroll__items-list' ).slick({
+			infinite: false,
+			arrows: false,
+			dots: true,
+			mobileFirst: true,
+			centerMode: true
+		});
+	}
+}
+
+new BrowseScroll();
