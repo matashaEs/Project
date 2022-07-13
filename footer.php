ef8c8421ebf60e5453e7ab86037415cda77e20ab
matashaEs <natali.babinska@ukr.net>
2022-07-13 19:37:53 +0200

    add  !empty

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
 <?php
-$social_media = get_field('social_media', 'options') ?? false;
+$social_media = get_field( 'social_media', 'options' ) ?? false;
 
-if (have_rows('contact_information', 'option')) :
-	while (have_rows('contact_information', 'option')): the_row();
-		$address = get_sub_field('address');
-		$phone_first = get_sub_field('phone_first');
-		$mail = get_sub_field('mail');
-		$country = get_sub_field('country');
+if ( have_rows( 'contact_information', 'option' ) ) :
+	while ( have_rows( 'contact_information', 'option' ) ): the_row();
+		$address     = get_sub_field( 'address' );
+		$phone_first = get_sub_field( 'phone_first' );
+		$mail        = get_sub_field( 'mail' );
+		$country     = get_sub_field( 'country' );
 	endwhile;
 endif;
 
@@ -16,31 +16,39 @@ endif;
 		<div class="container">
 			<div class="row footer__row">
 				<div class="footer__col">
-					<h5 class="footer__title"><?php _e('Connect', 'nuplo'); ?></h5>
+					<h5 class="footer__title"><?php _e( 'Connect', 'nuplo' ); ?></h5>
 				</div>
 				<div class="footer__col">
 					<div class="footer__content">
-						<p><?= $address ?></p>
-						<p><?= $country ?></p>
+						<?php if ( ! empty( $address ) ) : ?>
+							<p><?= $address ?></p>
+						<?php endif; ?>
+						<?php if ( ! empty( $country ) ) : ?>
+							<p><?= $country ?></p>
+						<?php endif; ?>
 					</div>
 					<div class="footer__content">
-						<p><a href="mailto: <?= $mail ?>"> <?= $mail ?></a></p>
-						<p><?= $phone_first ?></p>
+						<?php if ( ! empty( $mail ) ) : ?>
+							<p><a href="mailto: <?= $mail ?>"> <?= $mail ?></a></p>
+						<?php endif; ?>
+						<?php if ( ! empty( $phone_first ) ) : ?>
+							<p><?= $phone_first ?></p>
+						<?php endif; ?>
 					</div>
 					<?php
-					if ($social_media):
+					if ( $social_media ):
 						?>
 						<div class="footer__content">
 							<ul class="footer__social-media">
-								<?php foreach ($social_media as $item):
+								<?php foreach ( $social_media as $item ):
 									$icon = $item['title'] ?? false;
 									$target = $item['link'] ?? '#';
 
-									if ($icon):
+									if ( $icon ):
 										?>
 
 										<li class="footer__social-media-item">
-											<a href="<?php echo esc_url($target); ?>" target="_blank">
+											<a href="<?php echo esc_url( $target ); ?>" target="_blank">
 												<?= $icon ?>
 											</a>
 										</li>
@@ -59,24 +67,24 @@ endif;
 			</div>
 			<div class="row footer__row">
 				<div class="footer__col">
-					<h5 class="footer__title"><?php _e('Explore', 'nuplo'); ?></h5>
+					<h5 class="footer__title"><?php _e( 'Explore', 'nuplo' ); ?></h5>
 				</div>
 				<div class="footer__col">
 					<?php
-					wp_nav_menu(array(
-							'menu' => "footer-menu-mobile",
-							'menu_class' => "footer__menu",
+					wp_nav_menu( array(
+							'menu'           => "footer-menu-mobile",
+							'menu_class'     => "footer__menu",
 							'theme_location' => 'footer-menu-mobile',
-					));
+					) );
 					?>
 				</div>
 			</div>
 			<div class="row footer__row">
 				<div class="footer__col">
-					<h5 class="footer__title footer__title-cai">© CAI <?php echo date("Y"); ?></h5>
+					<h5 class="footer__title footer__title-cai">© CAI <?php echo date( "Y" ); ?></h5>
 				</div>
 				<div class="footer__col">
-					<p class="footer__small"><?php _e('Website Designed by BS LLC', 'nuplo'); ?></p>
+					<p class="footer__small"><?php _e( 'Website Designed by BS LLC', 'nuplo' ); ?></p>
 				</div>
 			</div>
 		</div>
@@ -87,25 +95,25 @@ endif;
 		<div class="container">
 			<div class="row footer__row">
 				<div class="footer__col">
-					<?= get_svg('cai-logo.svg', 'footer__logo'); ?>
-					<p class="footer__big"><?php _e('Industry-specific, mission-critical enterprise and warehouse
-					management software.', 'nuplo'); ?></p>
+					<?= get_svg( 'cai-logo.svg', 'footer__logo' ); ?>
+					<p class="footer__big"><?php _e( 'Industry-specific, mission-critical enterprise and warehouse
+					management software.', 'nuplo' ); ?></p>
 					<div class="footer__flex footer__flex--first">
 						<?php
-						if ($social_media):
+						if ( $social_media ):
 							?>
 							<ul class="footer__social-media">
-								<?php foreach ($social_media as $item):
+								<?php foreach ( $social_media as $item ):
 									$icon = $item['title'] ?? false;
 									$target = $item['link'] ?? '#';
 									$file = get_template_directory() . "/assets/img/icon-" . $icon . ".svg";
 
-									if ($icon):
+									if ( $icon ):
 										?>
 
 										<li class="footer__social-media-item">
-											<a href="<?php echo esc_url($target); ?>" target="_blank">
-												<?php echo file_get_contents($file, true); ?>
+											<a href="<?php echo esc_url( $target ); ?>" target="_blank">
+												<?php echo file_get_contents( $file, true ); ?>
 											</a>
 										</li>
 
@@ -117,34 +125,40 @@ endif;
 
 						<?php
 						endif;
-						wp_nav_menu(array(
-								'menu' => "footer-menu",
-								'menu_class' => "footer__menu",
+						wp_nav_menu( array(
+								'menu'           => "footer-menu",
+								'menu_class'     => "footer__menu",
 								'theme_location' => 'footer-menu',
-						));
+						) );
 						?>
 					</div>
 				</div>
 				<div class="footer__col">
-					<h5 class="footer__title"><?php _e('Get in touch', 'nuplo'); ?></h5>
+					<h5 class="footer__title"><?php _e( 'Get in touch', 'nuplo' ); ?></h5>
 					<div class="footer__flex">
-						<p class="footer__mail"><a href="mailto: <?= $mail ?>"> <?= $mail ?></a></p>
-						<a class="button button--footer footer__button"><?php _e('Live Chat', 'nuplo'); ?></a>
+						<?php if ( ! empty( $mail ) ) : ?>
+							<p class="footer__mail"><a href="mailto: <?= $mail ?>"> <?= $mail ?></a></p>
+						<?php endif; ?>
+						<a class="button button--footer footer__button"><?php _e( 'Live Chat', 'nuplo' ); ?></a>
 					</div>
-					<h5 class="footer__title"><?php _e('Explore', 'nuplo'); ?></h5>
+					<h5 class="footer__title"><?php _e( 'Explore', 'nuplo' ); ?></h5>
 					<div class="footer__menu-explore">
 						<?php
-						wp_nav_menu(array(
-								'menu' => "footer-menu-explore",
-								'menu_class' => "footer__menu",
+						wp_nav_menu( array(
+								'menu'           => "footer-menu-explore",
+								'menu_class'     => "footer__menu",
 								'theme_location' => 'footer-menu-explore',
-						));
+						) );
 						?>
 					</div>
 					<div class="footer__flex">
-						<p class="footer__address"><?= $address ?></p>
-						<p class="footer__phone"><?= $phone_first ?></p>
-						<p class="footer__cai">© CAI <?php echo date("Y"); ?></p>
+						<?php if ( ! empty( $address ) ) : ?>
+							<p class="footer__address"><?= $address ?></p>
+						<?php endif; ?>
+						<?php if ( ! empty( $phone_first ) ) : ?>
+							<p class="footer__phone"><?= $phone_first ?></p>
+						<?php endif; ?>
+						<p class="footer__cai">© CAI <?php echo date( "Y" ); ?></p>
 					</div>
 				</div>
 			</div>
