56af1d3b816f77bf6e520e4ea4d25201ff91954c
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-07-19 18:32:12 +0200

    templates, extended menus, google maps api key

--- inc/classes/class-hooks.php
+++ inc/classes/class-hooks.php
@@ -7,6 +7,7 @@ class Hooks {
 	public function __construct() {
 		add_action( 'excerpt_more', [ $this, 'set_excerpt_content' ] );
 		add_filter( 'nav_menu_css_class', [ $this, 'add_additional_class_on_li' ], 5, 3 );
+		add_action( 'acf/init', [ $this, 'acf_map_api' ] );
 	}
 
 	/**
@@ -24,5 +25,18 @@ class Hooks {
 		}
 
 		return $classes;
+	}
+
+	/**
+	 * Update ACF settings with Google Maps API key from Options Page
+	 */
+	public function acf_map_api() {
+		$google_maps_options = get_field( 'google_maps', 'option' );
+		$google_maps_api     = '';
+		if ( ! empty( $google_maps_options ) ) :
+			$google_maps_api = $google_maps_options['google_maps_api_key'] ?? '';
+		endif;
+
+		acf_update_setting( 'google_api_key', $google_maps_api );
 	}
 }
