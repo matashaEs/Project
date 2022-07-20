86b462caf47b5736e8278ee31e77c62d695a44b7
Mariia Maliuk <>
2022-07-20 20:21:05 +0200

    created menu

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -3,32 +3,45 @@ import $ from 'jquery';
 class Navigation {
     constructor() {
         this.body = $( 'body' );
-        $('.menu-item-has-children > a').click(function (e) {
-            e.preventDefault();
-            $(this).next('.sub-menu').slideToggle();
-            $('.menu-item-has-children').toggleClass('nav__mobile-menu-item--chosen')
-        });
+        if ( 1023 > $( window ).width() ) {
+            $( '.menu-item-has-children > a' ).click( function( e ) {
+                e.preventDefault();
+                $( this ).next( '.sub-menu' ).slideToggle();
+                $( '.menu-item-has-children' ).toggleClass( 'nav__mobile-menu-item--chosen' );
+            });
+        } else {
+            $( '.menu-item-has-children > a' ).click( function( e ) {
+                e.preventDefault();
+            });
+        }
 
 
         $( '.nav__open' ).on( 'click', this.toggleMenuNavOpen );
         $( '.nav__close' ).on( 'click', this.toggleMenuNavClose );
+        $( '.nav__main-menu--open' ).on( 'click', this.toggleMenuNavOpen );
     }
 
     toggleMenuNavOpen = () => {
-        if ($(window).width() < 1023) {
-            $(".nav__open").removeClass('nav__mobile--visible')
-            $(".nav__mobile-menu").toggleClass('nav__mobile-menu--visible')
-            $(".nav__fluid").toggleClass('nav__mobile--scrolling')
-            $("body").addClass('home--no-scrolling')
+        if ( 1023 > $( window ).width() ) {
+            $( '.nav__open' ).removeClass( 'nav__mobile--visible' );
+            $( '.nav__mobile-menu' ).toggleClass( 'nav__mobile-menu--visible' );
+            $( '.nav__fluid' ).toggleClass( 'nav__mobile--scrolling' );
+            $( 'body' ).addClass( 'home-no-scrolling' );
+        } else {
+            $( '.nav__main-menu' ).removeClass( 'nav__main-menu--visible' );
+            $( '.nav__desktop--extended' ).toggleClass( 'nav--visible' );
         }
     }
 
     toggleMenuNavClose = () => {
-        if ($(window).width() < 1023) {
-            $(".nav__open").toggleClass('nav__mobile--visible')
-            $(".nav__mobile-menu").removeClass('nav__mobile-menu--visible')
-            $(".nav__fluid").removeClass('nav__mobile--scrolling')
-            $("body").removeClass('home--no-scrolling')
+        if ( 1023 > $( window ).width() ) {
+            $( '.nav__open' ).toggleClass( 'nav__mobile--visible' );
+            $( '.nav__mobile-menu' ).removeClass( 'nav__mobile-menu--visible' );
+            $( '.nav__fluid' ).removeClass( 'nav__mobile--scrolling' );
+            $( 'body' ).removeClass( 'home-no-scrolling' );
+        } else {
+            $( '.nav__main-menu' ).toggleClass( 'nav__main-menu--visible' );
+            $( '.nav__desktop--extended' ).removeClass( ' nav--visible' );
         }
     }
 }
--- template-parts/nav.php
+++ template-parts/nav.php
@@ -24,18 +24,18 @@ endif;
 						<a href="<?= esc_url( get_home_url() ); ?>" class="nav-logo">
 							<?php the_svg( 'cai-logo.svg', 'nav__logo' ); ?> </a>
 						<div class="nav__close-exit">
-							<h5 class="nav__close-text"><?= __( 'Exit' ) ?></h5>
+							<h5 class="nav__close-text"><?php _e( 'Exit', 'nuplo' ); ?></h5>
 							<?php the_svg( 'nav-close.svg', 'nav__close-icon' ); ?>
 						</div>
 					</div>
 					<div class="nav__info">
-                        <div class="nav__search-form">
-                            <form action="<?php echo esc_url( site_url( '/' ) ); ?>" type="GET">
-                                <input type="text" id="searchField" name="s" placeholder="" class="input nav__input">
-                                <div id="searchResults">
-                                </div>
-                            </form>
-                        </div>
+						<div class="nav__search-form">
+							<form action="<?php echo esc_url( site_url( '/' ) ); ?>" type="GET">
+								<input type="text" id="searchField" name="s" placeholder="" class="input nav__input">
+								<div id="searchResults">
+								</div>
+							</form>
+						</div>
 						<?php if ( ! empty( $title_nav ) ) : ?>
 							<h1> <?= esc_html( $title_nav ) ?> </h1>
 						<?php endif; ?>
@@ -63,7 +63,7 @@ endif;
 					</div>
 				</div>
 			</div>
-			<div class="nav__main-menu nav__desktop">
+			<div class="nav__main-menu nav__desktop nav__main-menu--visible">
 				<a href="<?= esc_url( get_home_url() ); ?>" class="nav-logo">
 					<?php the_svg( 'cai-logo.svg', 'nav__logo' ); ?>
 					<div class="nav__main-menu-items">
@@ -77,8 +77,59 @@ endif;
 							]
 						);
 						?>
-						<?php the_svg( 'nav-open.svg', 'nav-open' ); ?>
+						<div class="nav__main-menu--open">
+							<?php the_svg( 'nav-open.svg', 'nav-open' ); ?>
+						</div>
 					</div>
+			</div>
+			<div class="nav__desktop--extended">
+				<div class="nav__close">
+					<a href="<?= esc_url( get_home_url() ); ?>" class="nav-logo">
+						<?php the_svg( 'cai-logo.svg', 'nav__logo' ); ?> </a>
+					<div class="nav__close-exit">
+						<h5 class="nav__close-text"><?php _e( 'Exit', 'nuplo' ); ?></h5>
+						<?php the_svg( 'nav-close.svg', 'nav__close-icon' ); ?>
+					</div>
+				</div>
+				<div class="nav__desktop-content">
+					<div class="nav__desktop-text">
+						<?php if ( ! empty( $title_nav ) ) : ?>
+							<h3 class="nav__desktop-title"> <?= esc_html( $title_nav ) ?> </h3>
+						<?php endif; ?>
+						<?php if ( ! empty( $description ) ) : ?>
+							<p class="nav__desktop-description"> <?= esc_html( $description ) ?> </p>
+						<?php endif; ?>
+						<div class="nav__buttons">
+							<?php if ( ! empty( $buttons ) ) : ?>
+								<?php foreach ( $buttons as $button ) : ?>
+									<a href="<?= esc_url( $button['button']['url'] ) ?>" class="button p nav__button">
+										<?= esc_html( $button['button']['title'] ) ?>
+									</a>
+								<?php endforeach; ?>
+							<?php endif; ?>
+						</div>
+					</div>
+					<div class="nav__desktop-menu">
+							<form action="<?php echo esc_url( site_url( '/' ) ); ?>" type="GET" class="nav__desktop-form">
+								<input type="text" id="searchField" name="s" placeholder="Search" class="nav-input input">
+								<input type="submit" value="<?php _e( 'Submit', 'nuplo' ); ?>" id="searchButton" class="button nav-button">
+								<div id="searchResults">
+								</div>
+							</form>
+						<div class="nav__desktop-menu-items">
+							<?php
+							wp_nav_menu(
+								[
+									'theme_location' => 'primary-menu-extended',
+									'container'      => false,
+									'items_wrap'     => '%3$s',
+									'add_li_class'   => 'nav__desktop-menu-extended-item h5',
+								]
+							);
+							?>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
