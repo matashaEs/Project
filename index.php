695848602d50abc49db96b04dab873e068624679
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-07-20 18:52:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,5 @@
 get_header();
 
 the_content();
-$pages_data = apply_filters( 'cai_get_sidebar_and_content_data', null );
-get_template_part( 'template-parts/sidebar-and-content', null, $pages_data[1] );
 
 get_footer();
