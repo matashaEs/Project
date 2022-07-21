466892c93667360fa4294e507426be15d16f2a33
Mariia Maliuk <>
2022-07-21 12:32:22 +0200

    changes after code review

--- src/js/mobile-nav.js
+++ src/js/mobile-nav.js
@@ -3,46 +3,32 @@ import $ from 'jquery';
 class Navigation {
     constructor() {
         this.body = $( 'body' );
-        if ( 1023 > $( window ).width() ) {
-            $( '.menu-item-has-children > a' ).click( function( e ) {
-                e.preventDefault();
+        $( '.menu-item-has-children > a' ).click( function( e ) {
+            e.preventDefault();
+            if ( 1023 > $( window ).width() ) {
                 $( this ).next( '.sub-menu' ).slideToggle();
                 $( '.menu-item-has-children' ).toggleClass( 'nav__mobile-menu-item--chosen' );
-            });
-        } else {
-            $( '.menu-item-has-children > a' ).click( function( e ) {
-                e.preventDefault();
-            });
-        }
+            }
+        });
 
+        $( '.nav__open' ).on( 'click', this.toggleMenu );
+        $( '.nav__close' ).on( 'click', this.toggleMenu );
+        $( '.nav__main-menu--open' ).on( 'click', this.toggleMenu );
 
-        $( '.nav__open' ).on( 'click', this.toggleMenuNavOpen );
-        $( '.nav__close' ).on( 'click', this.toggleMenuNavClose );
-        $( '.nav__main-menu--open' ).on( 'click', this.toggleMenuNavOpen );
+        $( '.nav__open .nav__main-menu--open' ).on( 'click', ( e ) => {
+                this.showSidebarOnMobile( e, this );
+            }
+        );
     }
 
-    toggleMenuNavOpen = () => {
-        if ( 1023 > $( window ).width() ) {
-            $( '.nav__open' ).removeClass( 'nav__mobile--visible' );
-            $( '.nav__mobile-menu' ).toggleClass( 'nav__mobile-menu--visible' );
-            $( '.nav__fluid' ).toggleClass( 'nav__mobile--scrolling' );
-            $( 'body' ).addClass( 'home-no-scrolling' );
-        } else {
-            $( '.nav__main-menu' ).removeClass( 'nav__main-menu--visible' );
-            $( '.nav__desktop--extended' ).toggleClass( 'nav--visible' );
-        }
-    }
-
-    toggleMenuNavClose = () => {
-        if ( 1023 > $( window ).width() ) {
-            $( '.nav__open' ).toggleClass( 'nav__mobile--visible' );
-            $( '.nav__mobile-menu' ).removeClass( 'nav__mobile-menu--visible' );
-            $( '.nav__fluid' ).removeClass( 'nav__mobile--scrolling' );
-            $( 'body' ).removeClass( 'home-no-scrolling' );
-        } else {
-            $( '.nav__main-menu' ).toggleClass( 'nav__main-menu--visible' );
-            $( '.nav__desktop--extended' ).removeClass( ' nav--visible' );
-        }
+    toggleMenu = () => {
+        $( '.nav__open' ).toggleClass( 'nav__mobile--visible' );
+        $( '.nav__mobile-menu' ).toggleClass( 'nav__mobile-menu--visible' );
+        $( '.nav__fluid' ).toggleClass( 'nav__mobile--scrolling' );
+        $( 'body' ).toggleClass( 'home-no-scrolling' );
+        $( '.nav__main-menu' ).toggleClass( 'nav__main-menu--visible' );
+        $( '.nav__desktop--extended' ).toggleClass( 'nav--visible' );
+        $( 'body' ).toggleClass( 'home-no-scrolling' );
     }
 }
 
