501453c942c327bbc7217a5d233ec785a53bc0bc
Mariia Maliuk <>
2022-07-22 16:09:08 +0200

    some more changes

--- blocks/hero-with-description/hero-with-description.php
+++ blocks/hero-with-description/hero-with-description.php
@@ -17,11 +17,9 @@ if ( ! empty( $block['id'] ) ) {
 
 <section class="container-fluid hwd">
 	<div class="hwd__image">
-		<img src="<?= esc_url( $background_image_mobile['sizes']['full_hd'] ); ?>"
-			 class="hwd__image-mobile">
+		<img src="<?= esc_url( $background_image_mobile['sizes']['full_hd'] ); ?>" class="hwd__image-mobile">
 	</div>
-	<div class="hwd__bg"
-		 style="background-image: url('<?= esc_url( $background_image['sizes']['full_hd'] ); ?>')"></div>
+	<div class="hwd__bg" style="background-image: url('<?= esc_url( $background_image['sizes']['full_hd'] ); ?>')"></div>
 	<div class="container">
 		<div class="hwd__content">
 			<div class="hwd__path p">
@@ -43,10 +41,9 @@ if ( ! empty( $block['id'] ) ) {
 				<?php $button_target = $button['target']; ?>
 				<?php if ( ! empty( $button_title ) && ! empty( $button_url ) ) : ?>
 					<div class="hwd__button">
-						<a
-						 href="<?= esc_url( $button_url ); ?>"
-						 class="button hwd__button-link p"
-						 target=" <?= esc_attr( $button_target ) ?>">
+						<a href="<?= esc_url( $button_url ); ?>"
+							class="button hwd__button-link p"
+							target=" <?= esc_attr( $button_target ) ?>">
 							<?= esc_html( $button_title ) ?>
 						</a>
 					</div>
