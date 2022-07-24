465963d57e23df2b3b74ceaf90ca4d49b2f4545b
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-07-24 16:45:38 +0300

    init

---
+++ blocks/recent-product-project/recent-product-project.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ *  $title
+ *  $project [ title, description, link, image ]
+ */
+
+
+if ( ! empty( $block['id'] ) ) {
+	extract( get_fields( $block['id'] ) );
+} else {
+	extract( $block['data'] );
+}
+?>
+
+<?php if ( ! empty( $project ) ) : ?>
+	<section class="container-fluid recent-product-project">
+		<div class="container">
+			<?php if ( ! empty( $title ) ) : ?>
+				<div class="h2 recent-product-project__title"><?= esc_html( $title ) ?></div>
+			<?php endif; ?>
+			<div class="recent-product-project__project">
+				<div class="recent-product-project__project-content">
+					<?php if ( ! empty( $project['title'] ) ) : ?>
+						<div class="h3 recent-product-project__project-title">
+							<?= esc_html( $project['title'] ) ?>
+						</div>
+					<?php endif; ?>
+					<?php if ( ! empty( $project['description'] ) ) : ?>
+						<div class="p recent-product-project__project-description">
+							<?= esc_html( $project['description'] ) ?>
+						</div>
+					<?php endif; ?>
+					<?php if ( ! empty( $project['link'] ) ) : ?>
+						<a href="<?= esc_url( $project['link']['url'] ) ?>" class="button recent-product-project__project-link">
+							<?= esc_html( $project['link']['title'] ) ?>
+						</a>
+					<?php endif; ?>
+				</div>
+				<div class="recent-product-project__project-image-container">
+					<img src="<?= esc_url( $project['image']['sizes']['full_hd'] ); ?>" alt="project_image"
+						class="recent-product-project__project-image">
+				</div>
+			</div>
+		</div>
+	</section>
+<?php endif; ?>
