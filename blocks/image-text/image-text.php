222e5afd39f14ce2d9de01d2643d4ec821155d83
matashaEs <natali.babinska@ukr.net>
2022-07-27 16:05:23 +0200

    image-text for mobile

--- blocks/image-text/image-text.php
+++ blocks/image-text/image-text.php
@@ -6,7 +6,7 @@ if ( ! empty( $block['id'] ) ) {
 	extract( $block['data'] );
 }
 
-$class_reverse = ( 'reverse' === $direction ) ? 'image-text__reverse' : '';
+$class_reverse = ( 'reverse' === $direction ) ? 'image-text__row--reverse' : '';
 $class_color   = ( true === $add_background ) ? 'image-text__background' : '';
 
 ?>
@@ -14,32 +14,37 @@ $class_color   = ( true === $add_background ) ? 'image-text__background' : '';
 <section class="container-fluid image-text <?= esc_html( $class_color ) ?>">
 	<div class="container">
 		<div class="row image-text__row <?= esc_html( $class_reverse ) ?>">
+			<div class="image-text__column-content">
+				<?php if ( ! empty( $title ) ) : ?>
+				<div class="image-text__title-section">
+					<div class="image-text__title h2"><?= esc_html( $title ) ?></div>
+					<?php the_svg( 'blue-arrow.svg', 'image-text__arrow' ); ?>
+				</div>
+				<?php endif; ?>
+				<div class="image-text__columns-with-button">
+					<div class="image-text__columns">
+						<?php if ( ! empty( $description ) ) : ?>
+							<?php foreach ( $description as $content ) : ?>
+								<div class="image-text__column">
+									<?= wp_kses( $content['column'], [ 'p' => [ '' ] ] ) ?>
+								</div>
+							<?php endforeach; ?>
+						<?php endif; ?>
+					</div>
+					<?php if ( ! empty( $link['url'] && $link['title'] ) ) : ?>
+						<a
+								href="<?= esc_url( $link['url'] ); ?>"
+								class="button p image-text__button"
+								target="<?= esc_attr( $link['target'] ) ?>">
+							<?= esc_html( $link['title'] ); ?>
+						</a>
+					<?php endif; ?>
+				</div>
+			</div>
 			<div class="image-text__column-image">
 				<?php if ( ! empty( $image ) ) : ?>
 					<div class="image-text__image"
 						style="background-image: url('<?= esc_url( $image['sizes']['large'] ); ?>')"></div>
-				<?php endif; ?>
-			</div>
-			<div class="image-text__column-content">
-				<?php if ( ! empty( $title ) ) : ?>
-					<div class="image-text__title h2"><?= esc_html( $title ) ?></div>
-				<?php endif; ?>
-				<div class="image-text__columns">
-					<?php if ( ! empty( $description ) ) : ?>
-						<?php foreach ( $description as $content ) : ?>
-							<div class="image-text__column">
-								<?= wp_kses( $content['column'], [ 'p' => [ '' ] ] ) ?>
-							</div>
-						<?php endforeach; ?>
-					<?php endif; ?>
-				</div>
-				<?php if ( ! empty( $link['url'] && $link['title'] ) ) : ?>
-					<a
-							href="<?= esc_url( $link['url'] ); ?>"
-							class="button p image-text__button"
-							target="<?= esc_attr( $link['target'] ) ?>">
-						<?= esc_html( $link['title'] ); ?>
-					</a>
 				<?php endif; ?>
 			</div>
 		</div>
