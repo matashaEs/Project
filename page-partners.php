ecd5416fb9bbea410a2ee4c0127592c82281c433
Adam Basa <>
2022-08-02 13:06:47 +0200

    partners init

--- page-partners.php
+++ page-partners.php
@@ -1,27 +1,37 @@
-<?php /** Template Name: Partners Page */
-$partners = apply_filters( 'get_partners_list', null );
+<?php
+/** Template Name: Partners Page */
+$partners = apply_filters( 'cai_get_list_of_partners', null );
 
 get_header();
 
 ?>
 
-<div class="container-fluid">
-	<div class="container">
-		<h1><?php the_title(); ?></h1>
-		<?php
+	<div class="container-fluid">
+		<div class="container">
+			<?php
+			echo esc_html( get_template_part( 'template-parts/breadcrumbs' ) );
+			?>
+			<h1><?php the_title(); ?></h1>
+			<?php
 
-		if ( ! empty( $partners ) ) {
-			foreach ( $partners as $partner ) {
-				if ( ! empty( $partner ) ) {
-					?>
-					<a href="<?= esc_url( $partner['url'] ) ?>"><?= esc_html( $partner['name'] ) ?></a>
-					<?php
+			if ( ! empty( $partners ) ) {
+				foreach ( $partners as $partner ) {
+					if ( ! empty( $partner ) ) {
+						?>
+						<a href="<?= esc_url( $partner['url'] ) ?>"><?= esc_html( $partner['name'] ) ?></a>
+						<?php
+					}
 				}
+			} else {
+				?>
+				<h4>
+					<?php _e( 'No partners found...', 'nuplo' ); ?>
+				</h4>
+				<?php
 			}
-		}
-		?>
+			?>
+		</div>
 	</div>
-</div>
 
 <?php
 get_footer();
