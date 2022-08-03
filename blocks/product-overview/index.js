a1fdfcd3369454966667a2e5af986025dc52575f
Mariia Maliuk <>
2022-08-03 17:07:37 +0200

    created share button

--- blocks/product-overview/index.js
+++ blocks/product-overview/index.js
@@ -1,2 +1,33 @@
 import './product-overview.scss';
+import $ from 'jquery';
+import copy from 'copy-to-clipboard';
 
+class ProductOverview {
+    constructor() {
+        $( '.product-overview__share-option--copy' ).on( 'click', this.showSnackBar );
+        $( '.product-overview__button-share' ).on( 'click', () => {
+            $( '.product-overview__share' ).toggleClass( 'product-overview__share--active' );
+        });
+    }
+
+    showSnackBar() {
+        var snackButton = document.querySelector( '.product-overview__snackText' );
+        $( '.product-overview__snackText' ).css( 'display', 'block' );
+
+        setTimeout( () => {
+            copy( window.location.href );
+            $( '.product-overview__snackText' ).toggleClass( 'product-overview__snackText--show' );
+
+            setTimeout( function() {
+                snackButton.className = snackButton.className.replace( 'product-overview__snackText--show', '' );
+
+                setTimeout( () => {
+                    $( '.product-overview__snackText' ).css( 'display', 'none' );
+                }, 500 );
+            }, 3000 );
+        }, 100 );
+    }
+}
+
+
+new ProductOverview();
--- blocks/product-overview/product-overview.php
+++ blocks/product-overview/product-overview.php
@@ -12,42 +12,77 @@ if ( ! empty( $block['id'] ) ) {
 } else {
 	extract( $block['data'] );
 }
+
+$current_post_url = get_permalink();
 ?>
 
 <section class="container-fluid product-overview">
 	<?php if ( ! empty( $image ) || get_the_post_thumbnail_url() ) : ?>
-	<div class="product-overview__bg" style="background-image: url( '<?= ! empty( $image ) ? esc_url( $image['sizes']['full_hd'] ) : esc_url( get_the_post_thumbnail_url() ); ?> ' )">
-	</div>
+		<div class="product-overview__bg" style="background-image: url( '<?= ! empty( $image ) ? esc_url( $image['sizes']['full_hd'] ) : esc_url( get_the_post_thumbnail_url() ); ?> ' )">
+		</div>
 	<?php endif; ?>
 	<div class="product-overview__path">
 		<p> Products > ERP > Ponderosa ERP </p>
 	</div>
 	<div class="product-overview__title product-overview__title--mobile">
-		<h1> <?= esc_html( the_title() )?></h1>
+		<h1> <?= esc_html( the_title() ) ?></h1>
 	</div>
 	<div class="product-overview__title product-overview__title--desktop">
-		<h3> <?= esc_html( $title )?></h3>
+		<h3> <?= esc_html( $title ) ?></h3>
 	</div>
 	<?php if ( ! empty( $description ) ) : ?>
 		<div class="product-overview__description">
-			<p> <?= wp_kses( $description, [ 'br' => [] ] )?></p>
+			<p> <?= wp_kses( $description, [ 'br' => [] ] ) ?></p>
 		</div>
 	<?php endif ?>
 	<div class="product-overview__buttons">
 		<?php if ( ! empty( $buttons['buttons_with_text'] ) ) : ?>
-			<?php 
+			<?php
 			foreach ( $buttons['buttons_with_text'] as $button ) :
 				$button_with_text = $button['button_with_text'];
 				?>
 				<?php if ( ! empty( $button_with_text ) ) : ?>
 				<a href="<?= esc_url( $button_with_text['url'] ) ?>" class="button p">
-					<?= esc_html( $button_with_text['title'] )?>
+					<?= esc_html( $button_with_text['title'] ) ?>
 				</a>
-			<?php endif; ?>
-		<?php endforeach; ?>
+				<?php endif; ?>
+			<?php endforeach; ?>
 		<?php endif; ?>
 		<?php if ( 1 == $buttons ['add_share_button'] ) : ?>
-		<a href="" class="button product-overview__button-share"> <?php the_svg( 'share-button.svg', 'product-overview-share' ); ?></a>
+			<div class="button product-overview__button-share"> <?php the_svg( 'share-button.svg', 'product-overview-share' ); ?></div>
+				<div class="product-overview__share">
+					<div class="product-overview__share-heading p-large">
+						Share
+					</div>
+					<div class="product-overview__share-option">
+						<a href="<?= 'http://www.linkedin.com/shareArticle?mini=true&url=' . esc_url( $current_post_url ) ?>" class="product-overview__share-option--link">
+							<img src="<?= esc_url( get_template_directory_uri() . '/assets/img/share-linkedin.svg' ) ?>"
+								alt="CAI icon"> <p class="p-large product-overview__share-option--text"> Linkedin </p>
+						</a>
+					</div>
+					<div class="product-overview__share-option">
+						<a href="<?= 'http://www.facebook.com/sharer.php?u=' . esc_url( $current_post_url ) ?>" class="product-overview__share-option--link">
+							<img src="<?= esc_url( get_template_directory_uri() . '/assets/img/share-facebook.svg' ) ?>"
+								alt="CAI icon"> <p class="p-large product-overview__share-option--text"> Facebook </p>
+						</a>
+					</div>
+					<div class="product-overview__share-option">
+						<a href="<?= 'mailto:?Subject=Arcadia Towers&Body=' . esc_url( $current_post_url ) ?>" class="product-overview__share-option--link">
+							<img src="<?= esc_url( get_template_directory_uri() . '/assets/img/share-email.svg' ) ?>"
+								alt="CAI icon"> <p class="p-large product-overview__share-option--text"> Email </p>
+						</a>
+					</div>
+					<div class="product-overview__share-option">
+						<div class="product-overview__share-option--link product-overview__share-option--copy">
+							<img src="<?= esc_url( get_template_directory_uri() . '/assets/img/share-copy-link.svg' ) ?>"
+								alt="CAI icon"> <p class="p-large product-overview__share-option--text"> Copy Link </p>
+						</div>
+					</div>
+				</div>
+			<div class="product-overview__snackText">
+				<?php _e( "The website's address is copied...", 'nuplo' ); ?>
+			</div>
+
 		<?php endif; ?>
 	</div>
 </section>
