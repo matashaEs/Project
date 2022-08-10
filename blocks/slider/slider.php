b3501086ef40c9f3cff3af44898c010bbd2a4fa6
Mariia Maliuk <>
2022-08-10 18:10:57 +0200

    changes after cr

--- blocks/slider/slider.php
+++ blocks/slider/slider.php
@@ -15,10 +15,12 @@ if ( ! empty( $block['id'] ) ) {
 	<div class="container slider__container">
 		<?php if ( ! empty( $slider ) ) : ?>
 			<?php foreach ( $slider as $slider_item ) : ?>
-                <div class="slider__item">
-                    <img src="<?= esc_url( $slider_item['image']['sizes']['full_hd'] );?>"
-                    alt="CAI" class="slider__image">
-                </div>
+				<?php if ( ! empty( $slider_item ) ) : ?>
+					<div class="slider__item">
+						<img src="<?= esc_url( $slider_item['image']['sizes']['full_hd'] );?>"
+						alt="CAI" class="slider__image">
+					</div>
+				<?php endif; ?>
 			<?php endforeach; ?>
 		<?php endif; ?>
 	</div>
