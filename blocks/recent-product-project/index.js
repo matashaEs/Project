4c2dcc67b1eb8f18af1bc8e7f88273d717bc2322
matashaEs <natali.babinska@ukr.net>
2022-08-12 18:34:53 +0200

    done

--- blocks/recent-product-project/index.js
+++ blocks/recent-product-project/index.js
@@ -1,1 +1,28 @@
 import './recent-product-project.scss';
+
+import $ from 'jquery';
+
+
+class RecentProductProject {
+    constructor() {
+        this.slider();
+    }
+
+    slider() {
+        $( '.recent-product-project__slider' ).slick({
+            infinite: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+
+            // variableWidth: true,
+            dots: true,
+            centerMode: false,
+            autoplay: false,
+            autoplaySpeed: 2000,
+            pauseOnFocus: false,
+        });
+    }
+}
+
+new RecentProductProject();
--- blocks/recent-product-project/recent-product-project.php
+++ blocks/recent-product-project/recent-product-project.php
@@ -1,7 +1,7 @@
 <?php
 /**
  *  $title
- *  $project [ title, description, link, image ]
+ *  $projects [ title, description, link, image ]
  *  $background_color [ select: 'default', 'modular--white', 'modular--off-white' ]
  */
 
@@ -13,35 +13,39 @@ if ( ! empty( $block['id'] ) ) {
 }
 ?>
 
-<?php if ( ! empty( $project ) ) : ?>
+<?php if ( ! empty( $projects ) ) : ?>
 	<section class="container-fluid recent-product-project modular<?= ! empty( $background_color ) ? ' ' . esc_html( $background_color ) : ''?>">
 		<div class="container">
 			<?php if ( ! empty( $title ) ) : ?>
 				<div class="row h2 recent-product-project__title"><?= esc_html( $title ) ?></div>
 			<?php endif; ?>
-			<div class="row recent-product-project__project modular__item--no-mobile">
-				<div class="recent-product-project__project-content">
-					<?php if ( ! empty( $project['title'] ) ) : ?>
-						<div class="h3 recent-product-project__project-title">
-							<?= esc_html( $project['title'] ) ?>
-						</div>
-					<?php endif; ?>
-					<?php if ( ! empty( $project['description'] ) ) : ?>
-						<div class="p recent-product-project__project-description">
-							<?= esc_html( $project['description'] ) ?>
-						</div>
-					<?php endif; ?>
-					<?php if ( ! empty( $project['link'] ) ) : ?>
-						<a href="<?= esc_url( $project['link']['url'] ) ?>"
-							class="button recent-product-project__project-button">
-							<?= esc_html( $project['link']['title'] ) ?>
-						</a>
+			<div class=" recent-product-project__slider">
+			<?php foreach ( $projects as $project ) : ?>
+				<div class="row recent-product-project__project modular__item--no-mobile">
+					<div class="recent-product-project__project-content">
+						<?php if ( ! empty( $project['title'] ) ) : ?>
+							<div class="h3 recent-product-project__project-title">
+								<?= esc_html( $project['title'] ) ?>
+							</div>
+						<?php endif; ?>
+						<?php if ( ! empty( $project['description'] ) ) : ?>
+							<div class="p recent-product-project__project-description">
+								<?= esc_html( $project['description'] ) ?>
+							</div>
+						<?php endif; ?>
+						<?php if ( ! empty( $project['link'] ) ) : ?>
+							<a href="<?= esc_url( $project['link']['url'] ) ?>"
+								class="button recent-product-project__project-button">
+								<?= esc_html( $project['link']['title'] ) ?>
+							</a>
+						<?php endif; ?>
+					</div>
+					<?php if ( ! empty( $project['image'] ) ) : ?>
+						<div class="recent-product-project__project-image"
+							style="background-image: url('<?= esc_url( $project['image']['sizes']['full_hd'] ); ?>')"></div>
 					<?php endif; ?>
 				</div>
-				<?php if ( ! empty( $project['image'] ) ) : ?>
-					<div class="recent-product-project__project-image"
-						style="background-image: url('<?= esc_url( $project['image']['sizes']['full_hd'] ); ?>')"></div>
-				<?php endif; ?>
+			<?php endforeach; ?>
 			</div>
 		</div>
 	</section>
