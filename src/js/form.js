5e714b80ea8c7cae2978464bc440b55d6aee6041
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-08-18 23:18:02 +0200

    thank you

--- src/js/form.js
+++ src/js/form.js
@@ -4,34 +4,13 @@ class Form {
     parent = null;
     optionsContainer = null;
 
-    errors = {
-        empty: 'Field is required',
-        short: {
-
-        },
-        invalid: {
-            text: {
-                name: 'Name is invalid',
-                firstName: 'First Name is invalid',
-                lastName: 'Last Name is invalid',
-                phone: 'Phone is invalid',
-                email: 'Email is invalid',
-                companyName: 'Company Name is invalid',
-                Address: 'Address is invalid',
-                Message: 'Message is invalid',
-                jobTitle: 'Job Title is invalid',
-            }
-        }
-    }
-
     regexes = {
         firstname: /^[\w ]{2,30}$/,
         lastname: /^(\w([-']?\w+)*)+$/,
         email: /^[a-zA-Z\d]([.!#$%&‘*+/=?^_`{|}~-]*[a-zA-Z\d]*)*[a-zA-Z\d]@[a-zA-Z\d](?:[a-zA-Z\d-]{0,61}[a-zA-Z\d])?(?:\.[a-zA-Z\d](?:[a-zA-Z\d-]{0,61}[a-zA-Z\d])?)*$/,
-        companyName: /^.{2,150}/,
+        company: /^.{2,150}/,
         message: /^.{2,150}/,
         jobTitle: /^.{2,150}/,
-        product: /^.{2,150}/,
     }
 
     constructor() {
@@ -48,6 +27,13 @@ class Form {
         $( '.button__send-form' ).on( 'click', ( e ) => {
             this.sendDataFromForm ( e, this );
         });
+
+        $( '.input' ).focus( function() {
+            if ( $( this ).hasClass( 'input--error' ) ) {
+                $( this ).removeClass( 'input--error' );
+                $( this ).next().removeClass( 'input__error--show' ).empty();
+            }
+        });
     }
 
     validateField( field ) {
@@ -59,10 +45,10 @@ class Form {
                 case 'firstname':   return this.regexes.firstname.test( field.val() );
                 case 'lastname':    return this.regexes.lastname.test( field.val() );
                 case 'email':       return this.regexes.email.test( field.val() );
-                case 'companyName': return this.regexes.companyName.test( field.val() );
+                case 'company': return this.regexes.company.test( field.val() );
                 case 'message':     return this.regexes.message.test( field.val() );
                 case 'jobTitle':    return this.regexes.jobTitle.test( field.val() );
-                case 'product':     return this.regexes.product.test( field.val() );
+                case 'product':     return true;
                 default:            return false;
             }
         }
@@ -73,18 +59,22 @@ class Form {
 
         let allFieldsAreValid = true;
 
-        let fields = this.parent.find( '.input' );
-        let errorFields = this.parent.find( '.input__error' );
-
-        errorFields.removeClass( 'input__error--show' ).empty();
+        let fields = this.parent.find( '.input' ).removeClass( 'input--error' );
+        this.parent.find( '.input__error' ).removeClass( 'input__error--show' ).empty();
 
         fields.each( function( ) {
-            const errorField = $( this ).next();
+            const errorField = 0 !== $( this ).next().length ? $( this ).next() :  $( this ).closest( '.select' ).next();
             const fieldName = $( this ).attr( 'placeholder' );
 
             if ( '' === $( this ).val() ) {
                 errorField.addClass( 'input__error--show' ).append( fieldName  + ' is required' );
-                $( this ).addClass( 'input--error' );
+
+                if ( 'hidden' !== $( this ).attr( 'type' ) || 'TEXTAREA' === $( this ).prop( 'tagName' ) ) {
+                    $( this ).addClass( 'input--error' );
+                } else {
+                    $( this ).closest( '.select' ).addClass( 'select--error' );
+                }
+
                 allFieldsAreValid = false;
             } else if ( ! outerValid( $( this ) ) ) {
                 errorField.addClass( 'input__terror--show' ).append( fieldName + ' is invalid' );
@@ -110,38 +100,51 @@ class Form {
         e.preventDefault();
         $this.parent = $( e.target.closest( 'form' ) );
 
-        const formData = $this.validateForm();
+        $this.parent.prev().addClass( 'hide' );
+        $this.parent.addClass( 'hide' );
 
-        if ( 'object' === typeof formData ) {
-            $this.parent.find( '.newsletter__form-valid' ).addClass( 'newsletter__form-valid--show' );
-            $this.parent.find( '.newsletter__form' ).addClass( 'newsletter__form--hide' );
-
-            let xhr = new XMLHttpRequest();
-            let url = 'https://api.hsforms.com/submissions/v3/integration/submit/' + $this.parent.attr( 'portal' ) + '/' + $this.parent.attr( 'id' );
-
-            let finalData = JSON.stringify( formData );
-
-            xhr.open( 'POST', url );
-
-            // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
-            xhr.setRequestHeader( 'Content-Type', 'application/json' );
-
-            xhr.onreadystatechange = function() {
-                if ( 4 === xhr.readyState && 200 === xhr.status ) {
-                    alert( xhr.responseText ); // Returns a 200 response if the submission is successful.
-                } else if ( 4 === xhr.readyState && 400 === xhr.status ) {
-                    alert( xhr.responseText ); // Returns a 400 error the submission is rejected.
-                } else if ( 4 === xhr.readyState && 403 === xhr.status ) {
-                    alert( xhr.responseText ); // Returns a 403 error if the portal isn't allowed to post submissions.
-                } else if ( 4 === xhr.readyState && 404 === xhr.status ) {
-                    alert( xhr.responseText ); //Returns a 404 error if the formGuid isn't found
-                }
-            };
-
-            // Sends the request
-
-            xhr.send( finalData );
+        if ( 1024 > window.innerWidth ) {
+            $this.parent.next()[0].scrollIntoView();
         }
+
+        setTimeout( function() {
+            $this.parent.next().addClass( 'form-valid--show' );
+        }, 500 );
+
+        // const formData = $this.validateForm();
+        //
+        // if ( 'object' === typeof formData ) {
+        //     let xhr = new XMLHttpRequest();
+        //     let url = 'https://api.hsforms.com/submissions/v3/integration/submit/' + $this.parent.attr( 'portal' ) + '/' + $this.parent.attr( 'id' );
+        //
+        //     let finalData = JSON.stringify( formData );
+        //
+        //     xhr.open( 'POST', url );
+        //
+        //     // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+        //     xhr.setRequestHeader( 'Content-Type', 'application/json' );
+        //
+        //     xhr.onreadystatechange = function() {
+        //         if ( 4 === xhr.readyState && 200 === xhr.status ) {
+        //
+        //             // Returns a 200 response if the submission is successful.
+        //             alert( xhr.responseText );
+        //             $this.parent.next().addClass( 'form-valid--show' );
+        //             $this.parent.prev().addClass( 'hide' );
+        //             $this.parent.addClass( 'hide' );
+        //         } else if ( 4 === xhr.readyState && 400 === xhr.status ) {
+        //             alert( xhr.responseText ); // Returns a 400 error the submission is rejected.
+        //         } else if ( 4 === xhr.readyState && 403 === xhr.status ) {
+        //             alert( xhr.responseText ); // Returns a 403 error if the portal isn't allowed to post submissions.
+        //         } else if ( 4 === xhr.readyState && 404 === xhr.status ) {
+        //             alert( xhr.responseText ); //Returns a 404 error if the formGuid isn't found
+        //         }
+        //     };
+        //
+        //     // Sends the request
+        //
+        //     xhr.send( finalData );
+        // }
     }
 
     /**
@@ -170,6 +173,11 @@ class Form {
                     }
                 }
             }
+        }
+
+        if ( this.parent.hasClass( 'select--form' ) && this.parent.hasClass( 'select--error' ) ) {
+            this.parent.removeClass( 'select--error' );
+            this.parent.next().removeClass( 'input__error--show' ).empty();
         }
     }
 
--- template-parts/form.php
+++ template-parts/form.php
@@ -11,9 +11,12 @@
  * $button
  *  $value
  *  $classes
+ * $after_sending
+ *  $title
+ *  $description
+ *  $link
  */
 extract( $args );
-
 ?>
 
 <form action="" method="post" class="form <?= ! empty( $form_classes ) ? ' ' . esc_attr( $form_classes ) : ''?>"
@@ -21,19 +24,23 @@ extract( $args );
 	<div class="input-form">
 		<?php foreach ( $fields as $field ) : ?>
 			<?php if ( ! empty( $field ) ) : ?>
-				<?php if ( 'textarea' === $field['type'] ) : ?>
-					<textarea
+			<div class="input-container <?= ! empty( $field['classes'] ) ? ' ' . esc_attr( $field['classes'] ) : ''?>">
+					<?php if ( 'textarea' === $field['type'] ) : ?>
+						<textarea
+								name="<?= esc_attr( $field['name'] ) ?>"
+								placeholder="<?= esc_attr( $field['placeholder'] ) ?>"
+								class="input input-message <?= ! empty( $field['classes'] ) ? ' ' . esc_attr( $field['classes'] ) : '' ?>"></textarea>
+					<?php elseif ( 'select' === $field['type'] ) : ?>
+						<?php get_template_part( 'template-parts/form-select', null, $field['data'] ); ?>
+					<?php else : ?>
+						<input
+							type="<?= esc_attr( $field['type'] ) ?>"
 							name="<?= esc_attr( $field['name'] ) ?>"
 							placeholder="<?= esc_attr( $field['placeholder'] ) ?>"
-							class="input <?= ! empty( $field['classes'] ) ? ' ' . esc_attr( $field['classes'] ) : '' ?>"></textarea>
-				<?php else : ?>
-					<input
-						type="<?= esc_attr( $field['type'] ) ?>"
-						name="<?= esc_attr( $field['name'] ) ?>"
-						placeholder="<?= esc_attr( $field['placeholder'] ) ?>"
-						class="input <?= ! empty( $field['classes'] ) ? ' ' . esc_attr( $field['classes'] ) : ''?>">
-			<?php endif; ?>
-			<div class="input__error"></div>
+							class="input">
+					<?php endif; ?>
+				<div class="p input__error"></div>
+			</div>
 		<?php endif; ?>
 		<?php endforeach; ?>
 	</div>
@@ -41,3 +48,23 @@ extract( $args );
 		<?= esc_attr( $button['value'] ) ?>
 	</button>
 </form>
+
+<?php if ( ! empty( $after_sending ) ) : ?>
+	<div class="form-valid">
+		<div class="form-valid__col">
+			<?php if ( ! empty( $after_sending['title'] ) ) : ?>
+				<div class="h1 form-valid__title"><?= esc_html( $after_sending['title'] ) ?></div>
+			<?php endif ?>
+		</div>
+		<div class="form-valid__col form-valid__col--right">
+			<?php if ( ! empty( $after_sending['description'] ) ) : ?>
+				<div class="p-large form-valid__description"><?= esc_html( $after_sending['description'] ) ?></div>
+			<?php endif ?>
+			<?php if ( ! empty( $after_sending['link'] ) ) : ?>
+				<a href="<?= esc_url( $after_sending['url'] ) ?>" class="button p form-valid__button">
+					<?= esc_html( $after_sending['link']['title'] ) ?>
+				</a>
+			<?php endif ?>
+		</div>
+	</div>
+<?php endif ?>
