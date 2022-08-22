1a58171fa735df586379b8939d1b2d5ab948bc14
matashaEs <natali.babinska@ukr.net>
2022-08-22 15:37:06 +0200

    get posts

--- blocks/news-section/news-section.php
+++ blocks/news-section/news-section.php
@@ -16,11 +16,7 @@ if ( ! empty( $block['id'] ) ) {
 	extract( $block['data'] );
 }
 
-$news = [
-	'date'  => 'January 11, 2022',
-	'title' => 'CAI Software Announces Majority Investment by Symphony Technology Group (STG)',
-	'link'  => 'google.com',
-];
+$i = 0;
 
 $section_background_color_class = '';
 
@@ -34,66 +30,75 @@ if ( ! empty( $background_color ) ) {
 $button_container_class = ! empty( $link_position ) ? ' news-section__row-button--left' : '';
 
 if ( is_single() && 'product' == get_post_type() ) {
-	$how_much_news = ! empty( $selected_news ) ? count( $selected_news ) : 2;
+	$how_much_news = 2;
 } else {
-	$how_much_news = ! empty( $selected_news ) ? count( $selected_news ) : 3;
+	$how_much_news = 3;
 }
 ?>
 
 <section class="container-fluid news-section modular<?= esc_html( $section_background_color_class ) ?>">
-	<div class="container news-section__container">
-		<div class="row news-section__row news-section__row-title">
+    <div class="container news-section__container">
+        <div class="row news-section__row news-section__row-title">
 			<?php if ( ! empty( $title ) ) : ?>
-				<div class="h2 news-section__title news-section__title--desktop"><?= esc_html( $title ); ?></div>
+                <div class="h2 news-section__title news-section__title--desktop"><?= esc_html( $title ); ?></div>
 				<?php if ( ! empty( $link && $link['url'] ) ) : ?>
-					<a href="<?= esc_url( $link['url'] ) ?>" class="h2 news-section__title news-section__title--link">
+                    <a href="<?= esc_url( $link['url'] ) ?>" class="h2 news-section__title news-section__title--link">
 						<?= esc_html( $title ); ?>
-					</a>
+                    </a>
 				<?php else : ?>
-					<div class="h2 news-section__title "><?= esc_html( $title ); ?></div>
+                    <div class="h2 news-section__title "><?= esc_html( $title ); ?></div>
 				<?php endif; ?>
 				<?php if ( is_single() && 'product' == get_post_type() && ! empty( $link ) ) : ?>
-					<a href="<?= esc_url( $link['url'] ) ?>" class="button news-section__button">
+                    <a href="<?= esc_url( $link['url'] ) ?>" class="button news-section__button">
 						<?= esc_html( $link['title'] ) ?>
-					</a>
+                    </a>
 				<?php endif; ?>
 			<?php endif; ?>
-		</div>
-		<?php if ( ! empty( $news ) ) : ?>
-			<div class="row news-section__row news-section__row-news">
-				<?php for ( $i = 0; $i < $how_much_news; $i ++ ) : ?>
-					<a href="<?= esc_url( $news['link'] ); ?>"
-						class="news-section__item modular__item--mobile">
-						<div class="news-section__item-top-content">
-							<?php if ( ! empty( $news['date'] ) ) : ?>
-								<div class="p news-section__item-date"><?= esc_html( $news['date'] ); ?></div>
-							<?php endif; ?>
-							<div class="news-section__item-categories">
-								<div class="news-section__item-category"></div>
-								<div class="news-section__item-category"></div>
-							</div>
-						</div>
-						<?php if ( ! empty( $news['title'] ) ) : ?>
-							<div class="h4 news-section__item-title"><?= esc_html( $news['title'] ); ?></div>
-						<?php endif; ?>
-						<?php if ( ! empty( $news['link'] ) ) : ?>
-							<div class="p news-section__item-read-more">
-								<?= esc_html( __( 'Read More', 'nuplo' ) ) ?>
-							</div>
-						<?php endif; ?>
-					</a>
-				<?php endfor; ?>
-			</div>
-		<?php endif; ?>
+        </div>
+
+
+        <div class="row news-section__row news-section__row-news">
+			<?php if ( ! empty( $selected_news ) ) : ?>
+				<?php $posts = apply_filters( 'cai_get_selected_news', $selected_news ); ?>
+			<?php elseif ( ! empty( $categories ) ) : ?>
+				<?php $category = get_term( $categories )->slug; ?>
+				<?php $posts = apply_filters( 'cai_get_filtered_news', $category ); ?>
+			<?php else : ?>
+				<?php $posts = apply_filters( 'cai_get_filtered_news', null ); ?>
+			<?php endif; ?>
+			<?php foreach ( $posts as $post ) { ?>
+                <a href="<?= esc_url( $post['url'] ); ?>"
+                   class="news-section__item modular__item--mobile">
+                    <div class="news-section__item-top-content">
+                        <div class="p news-section__item-date"><?= esc_html( $post['date'] ); ?></div>
+                        <div class="news__categories">
+							<?php foreach ( $post['categories'] as $category ) { ?>
+                                <object><a href="<?= esc_url( $category['slug'] ) ?>">
+                                        <div class="news__category"
+                                             style="background-color: <?= esc_html( $category['color'] ) ?>"></div>
+                                    </a></object>
+							<?php } ?>
+                        </div>
+                    </div>
+                    <div class="h4 news-section__item-title"><?= esc_html( $post['name'] ); ?></div>
+                    <div class="p news-section__item-read-more">
+						<?= esc_html( __( 'Read More', 'nuplo' ) ) ?>
+                    </div>
+                </a>
+				<?php if ( ++ $i == $how_much_news ) {
+					break;
+				} ?>
+			<?php } ?>
+        </div>
 		<?php if ( ! empty( $link ) ) : ?>
-			<div class="row news-section__row news-section__row-button <?= esc_html( $button_container_class ) ?>">
-				<a
-						href="<?= esc_url( $link['url'] ) ?>"
-						class="button news-section__button">
+            <div class="row news-section__row news-section__row-button <?= esc_html( $button_container_class ) ?>">
+                <a
+                        href="<?= esc_url( $link['url'] ) ?>"
+                        class="button news-section__button">
 					<?= esc_html( $link['title'] ) ?>
-				</a>
-			</div>
+                </a>
+            </div>
 		<?php endif; ?>
 
-	</div>
+    </div>
 </section>
