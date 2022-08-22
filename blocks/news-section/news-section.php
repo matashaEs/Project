f4d3afe461f55532399d8acd79a4ba71a6d4868a
matashaEs <natali.babinska@ukr.net>
2022-08-22 15:52:38 +0200

    linter

--- blocks/news-section/news-section.php
+++ blocks/news-section/news-section.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * $title [ text ]
- * $what_news [ select: 'all', 'selected' ]
+ * $what_news [ select: 'all', 'category', 'selected' ]
  * $selected_news [ repeater; shows if 'selected' == $what_news ]
  *     $news [ Post Type Object (CPT: Post) ]
+ * $categories [taxonomy; shows if 'category' == $what_news ]
  * $link [ link ]
  * $link_position [ true/false; shows if $link is not empty ]
  * $background_color [ select: 'default', 'modular--white', 'modular--off-white' ]
@@ -37,68 +38,68 @@ if ( is_single() && 'product' == get_post_type() ) {
 ?>
 
 <section class="container-fluid news-section modular<?= esc_html( $section_background_color_class ) ?>">
-    <div class="container news-section__container">
-        <div class="row news-section__row news-section__row-title">
+	<div class="container news-section__container">
+		<div class="row news-section__row news-section__row-title">
 			<?php if ( ! empty( $title ) ) : ?>
-                <div class="h2 news-section__title news-section__title--desktop"><?= esc_html( $title ); ?></div>
+				<div class="h2 news-section__title news-section__title--desktop"><?= esc_html( $title ); ?></div>
 				<?php if ( ! empty( $link && $link['url'] ) ) : ?>
-                    <a href="<?= esc_url( $link['url'] ) ?>" class="h2 news-section__title news-section__title--link">
+					<a href="<?= esc_url( $link['url'] ) ?>" class="h2 news-section__title news-section__title--link">
 						<?= esc_html( $title ); ?>
-                    </a>
+					</a>
 				<?php else : ?>
-                    <div class="h2 news-section__title "><?= esc_html( $title ); ?></div>
+					<div class="h2 news-section__title "><?= esc_html( $title ); ?></div>
 				<?php endif; ?>
 				<?php if ( is_single() && 'product' == get_post_type() && ! empty( $link ) ) : ?>
-                    <a href="<?= esc_url( $link['url'] ) ?>" class="button news-section__button">
+					<a href="<?= esc_url( $link['url'] ) ?>" class="button news-section__button">
 						<?= esc_html( $link['title'] ) ?>
-                    </a>
+					</a>
 				<?php endif; ?>
 			<?php endif; ?>
-        </div>
-
-
-        <div class="row news-section__row news-section__row-news">
+		</div>
+		<div class="row news-section__row news-section__row-news">
 			<?php if ( ! empty( $selected_news ) ) : ?>
-				<?php $posts = apply_filters( 'cai_get_selected_news', $selected_news ); ?>
+				<?php $news_posts = apply_filters( 'cai_get_selected_news', $selected_news ); ?>
 			<?php elseif ( ! empty( $categories ) ) : ?>
 				<?php $category = get_term( $categories )->slug; ?>
-				<?php $posts = apply_filters( 'cai_get_filtered_news', $category ); ?>
+				<?php $news_posts = apply_filters( 'cai_get_filtered_news', $category ); ?>
 			<?php else : ?>
-				<?php $posts = apply_filters( 'cai_get_filtered_news', null ); ?>
+				<?php $news_posts = apply_filters( 'cai_get_filtered_news', null ); ?>
 			<?php endif; ?>
-			<?php foreach ( $posts as $post ) { ?>
-                <a href="<?= esc_url( $post['url'] ); ?>"
-                   class="news-section__item modular__item--mobile">
-                    <div class="news-section__item-top-content">
-                        <div class="p news-section__item-date"><?= esc_html( $post['date'] ); ?></div>
-                        <div class="news__categories">
-							<?php foreach ( $post['categories'] as $category ) { ?>
-                                <object><a href="<?= esc_url( $category['slug'] ) ?>">
-                                        <div class="news__category"
-                                             style="background-color: <?= esc_html( $category['color'] ) ?>"></div>
-                                    </a></object>
+			<?php foreach ( $news_posts as $news_post ) { ?>
+				<a href="<?= esc_url( $news_post['url'] ); ?>"
+					class="news-section__item modular__item--mobile">
+					<div class="news-section__item-top-content">
+						<div class="p news-section__item-date"><?= esc_html( $news_post['date'] ); ?></div>
+						<div class="news__categories">
+							<?php foreach ( $news_post['categories'] as $category ) { ?>
+								<object><a href="<?= esc_url( $category['slug'] ) ?>">
+										<div class="news__category"
+											style="background-color: <?= esc_html( $category['color'] ) ?>"></div>
+									</a></object>
 							<?php } ?>
-                        </div>
-                    </div>
-                    <div class="h4 news-section__item-title"><?= esc_html( $post['name'] ); ?></div>
-                    <div class="p news-section__item-read-more">
+						</div>
+					</div>
+					<div class="h4 news-section__item-title"><?= esc_html( $news_post['name'] ); ?></div>
+					<div class="p news-section__item-read-more">
 						<?= esc_html( __( 'Read More', 'nuplo' ) ) ?>
-                    </div>
-                </a>
-				<?php if ( ++ $i == $how_much_news ) {
+					</div>
+				</a>
+				<?php
+				if ( ++ $i == $how_much_news ) {
 					break;
-				} ?>
+				}
+				?>
 			<?php } ?>
-        </div>
+		</div>
 		<?php if ( ! empty( $link ) ) : ?>
-            <div class="row news-section__row news-section__row-button <?= esc_html( $button_container_class ) ?>">
-                <a
-                        href="<?= esc_url( $link['url'] ) ?>"
-                        class="button news-section__button">
+			<div class="row news-section__row news-section__row-button <?= esc_html( $button_container_class ) ?>">
+				<a
+						href="<?= esc_url( $link['url'] ) ?>"
+						class="button news-section__button">
 					<?= esc_html( $link['title'] ) ?>
-                </a>
-            </div>
+				</a>
+			</div>
 		<?php endif; ?>
 
-    </div>
+	</div>
 </section>
