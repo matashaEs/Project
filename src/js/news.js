9e7521977dff295f4d215243d9b7527325671917
matashaEs <natali.babinska@ukr.net>
2022-08-23 11:43:38 +0200

    js fix

--- src/js/news.js
+++ src/js/news.js
@@ -1,26 +1,27 @@
-import $ from "jquery";
+import $ from 'jquery';
 
 class paginationInfiniteScroll {
   constructor() {
     this.isLoading = false;
-    this.loadMoreDOM = $("#load_more");
+    this.loadMoreDOM = $( '#load_more' );
 
     this.checkStatus();
 
-    if (0 < this.loadMoreDOM.length) {
+    if ( 0 < this.loadMoreDOM.length ) {
+
       // event triggered when user is close to the pagination section
-      $(window).scroll(this.checkScrollPosition);
+      $( window ).scroll( this.checkScrollPosition );
     }
   }
 
   checkScrollPosition = () => {
     if (
-      $(window).scrollTop() + $(window).height() >
+      $( window ).scrollTop() + $( window ).height() >
       this.loadMoreDOM.offset().top - 800
     ) {
       if (
-        !this.isLoading &&
-        parseInt(websiteData.max_page) > parseInt(websiteData.current_page)
+        ! this.isLoading &&
+        parseInt( websiteData.max_page ) > parseInt( websiteData.current_page )
       ) {
         this.isLoading = true;
         this.loadMore();
@@ -29,7 +30,7 @@ class paginationInfiniteScroll {
   };
 
   checkStatus = () => {
-    if (parseInt(websiteData.max_page) === parseInt(websiteData.current_page)) {
+    if ( parseInt( websiteData.max_page ) === parseInt( websiteData.current_page ) ) {
       this.loadMoreDOM.remove();
     }
 
@@ -39,21 +40,22 @@ class paginationInfiniteScroll {
   loadMore = () =>
     $.ajax({
       url: websiteData.ajaxUrl,
-      method: "GET",
+      method: 'GET',
       accepts: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       data: {
+
         // action: 'load_more_posts',
         nonce: websiteData.nonce,
         current_page: websiteData.current_page,
         query_vars: websiteData.query_vars,
       },
-      success: (res) => {
-        if (res.success) {
-          this.loadMoreDOM.before(res.data);
+      success: ( res ) => {
+        if ( res.success ) {
+          this.loadMoreDOM.before( res.data );
 
-          websiteData.current_page = parseInt(websiteData.current_page) + 1;
+          websiteData.current_page = parseInt( websiteData.current_page ) + 1;
         }
       },
       complete: this.checkStatus,
