ecd0580fa2e336c322aecaf13a575abb53fe0e8d
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-08-24 09:49:34 +0200

    download by product

--- template-parts/form-select.php
+++ template-parts/form-select.php
@@ -2,7 +2,7 @@
 /**
  * $name
  * $select_classes
- * $options [id, slug, name]
+ * $options [id, slug, name, download_form_guid, demo_form_guid ]
  * $button_classes
  * $title
  */
@@ -10,17 +10,22 @@ extract( $args );
 
 $all_select_classes  = ! empty( $expand_to_top ) ? esc_html( ' select--expand-on-top' ) : '';
 $all_select_classes .= ! empty( $select_classes ) ? ' ' . esc_html( $select_classes ) : '';
+
+$product_form = 'product-download' === $name ? 'download_form_guid' : ( 'product-request' === $name ? 'demo_form_guid' : '' );
 ?>
 
 <div class="select<?= ! empty( $all_select_classes ) ? esc_html( $all_select_classes ) : '' ?>">
 	<div class="select__box">
 		<div class="select__options">
 			<?php foreach ( $options as $option ) : ?>
+				<?php if ( empty( $product_form ) || ! empty( $option[ $product_form ] ) ) : ?>
+					<?php $option_guid = ! empty( $option[ $product_form ] ) ? ' formID' . '=' . $option[ $product_form ] : ''; ?>
 				<div class="select__option radio__container">
 					<div><input
 								type="radio"
 								class="input__radio input__radio--filters"
 								id="<?= esc_attr( $option['slug'] . '_' . $option['id'] ) ?>"
+								<?= ! empty( $option_guid ) ? esc_html( $option_guid ) : '' ?>
 								value="<?= esc_attr( $option['slug'] ) ?>"
 								name="<?= esc_attr( $name . '-radio' ) ?>"/>
 					</div>
@@ -28,6 +33,7 @@ $all_select_classes .= ! empty( $select_classes ) ? ' ' . esc_html( $select_clas
 						<?= esc_html( $option['name'] ) ?>
 					</label>
 				</div>
+			<?php endif; ?>
 			<?php endforeach; ?>
 		</div>
 		<div class="select__selected <?= ! empty( $button_classes ) ? esc_attr( $button_classes ) : '' ?>">
