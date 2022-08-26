d11b584db827549eeb481add7c3b1ceb21c73325
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-08-26 02:46:06 +0200

    share pop-up small correction

--- src/js/share.js
+++ src/js/share.js
@@ -3,25 +3,31 @@ import copy from 'copy-to-clipboard';
 
 class Share {
     constructor() {
+
+        $( '.share__option-link' ).not( '.share__option-copy' ).on( 'click', function() {
+            console.log( 'here' );
+            $( this ).closest( '.share' ).toggleClass( 'share--active' );
+        });
         $( '.share__option-copy' ).on( 'click', this.showSnackBar );
         $( '.button-share' ).on( 'click', () => {
             $( '.share' ).toggleClass( 'share--active' );
         });
     }
 
-    showSnackBar() {
-        let snackButton = document.querySelector( '.share__snackText' );
-        $( '.share__snackText' ).css( 'display', 'block' );
+    showSnackBar( e ) {
+        let snackText = $( e.target )
+            .closest( '.share' ).removeClass( 'share--active' )
+            .next().css( 'display', 'block' );
 
         setTimeout( () => {
             copy( window.location.href );
-            $( '.share__snackText' ).toggleClass( 'share__snackText--show' );
+            snackText.toggleClass( 'share__snackText--show' );
 
             setTimeout( function() {
-                snackButton.className = snackButton.className.replace( 'share__snackText--show', '' );
+                snackText.toggleClass( 'share__snackText--show' );
 
                 setTimeout( () => {
-                    $( '.share__snackText' ).css( 'display', 'none' );
+                    snackText.css( 'display', 'none' );
                 }, 500 );
             }, 3000 );
         }, 100 );
