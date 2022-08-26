0d0432aa3c486858f46576d74f615b51b7cc7115
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-08-26 02:00:02 +0200

    request a demo from category pagw

--- blocks/overview-wide/overview-wide.php
+++ blocks/overview-wide/overview-wide.php
@@ -54,7 +54,7 @@ if ( ! empty( $block['id'] ) ) {
 						?>
 						<?php if ( ! empty( $button_with_text ) ) : ?>
 						<a href="<?= esc_url( $button_with_text['url'] ) ?>"
-						class="overview-wide__buttons-button button p"
+						class="overview-wide__button button p"
 						target=" <?= esc_attr( $button_with_text['target'] ) ?>">
 							<?= esc_html( $button_with_text['title'] ) ?>
 						</a>
@@ -63,17 +63,30 @@ if ( ! empty( $block['id'] ) ) {
 				<?php endif; ?>
 
 				<?php
+				$request_demo_page_link = apply_filters( 'cai_get_request_a_demo_pade_url', null );
+				if (
+						! empty( $request_demo_page_link ) &&
+						! empty( $buttons ['add_request_a_demo_button'] ) &&
+						! empty( get_field( 'demo_form_guid', 'category_' . get_query_var( 'cat' ) ) ) ) :
+					$request_demo_page_link .= '?category=' . get_post( get_the_ID() )->post_name;
+					?>
+					<a href="<?= esc_url( $request_demo_page_link ) ?>" class="button p overview-wide__button">
+						<?= esc_html__( 'Request a Demo', 'nuplo' ) ?>
+					</a>
+				<?php endif; ?>
+
+				<?php
 				$download_page_link = apply_filters( 'cai_get_download_pade_url', null );
 				if ( ! empty( $download_page_link ) && ! empty( $buttons ['add_download_button'] ) ) :
 					$download_page_link .= '?category=' . get_queried_object()->slug;
 					?>
-					<a href="<?= esc_url( $download_page_link ) ?>" class="button p overview-wide__buttons-button">
-						<?= esc_html__( 'Download Datasheet', 'nuplo' ) ?>
+					<a href="<?= esc_url( $download_page_link ) ?>" class="button p overview-wide__button">
+						<?= esc_html__( 'Download Overview', 'nuplo' ) ?>
 					</a>
 				<?php endif; ?>
 
 				<?php if ( 1 == $buttons ['add_share_button'] ) : ?>
-					<div class="button button-share overview-wide__buttons-button overview-wide__button-share overview-wide__button-share--desktop">
+					<div class="button button-share overview-wide__button overview-wide__button-share overview-wide__button-share--desktop">
 						<?php the_svg( 'share-button.svg', 'product-overview__button--share' ); ?>
 						<?php get_template_part( 'template-parts/share', null ); ?>
 					</div>
