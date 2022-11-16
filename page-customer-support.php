0b6213cf42f7ec37f591d1907a04a82a2921bbaa
Wiktoria ;) <viktoryia.b.2002@gmail.com>
2022-11-16 16:29:40 +0100

    cleaning

--- page-customer-support.php
+++ page-customer-support.php
@@ -2,19 +2,28 @@
 get_header();
 ?>
 
-    <div class="container-fluid request-demo page-with-form customer-support-form">
-        <div class="container request-demo__container">
-			<?php echo esc_html( get_template_part( 'template-parts/template-heading' ) ); ?>
-            <script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
-            <script>
-                hbspt.forms.create({
-                    region: "na1",
-                    portalId: "21350294",
-                    formId: "2649435b-35e0-49da-b538-e3c3ad00c7c5"
-                });
-            </script>
-        </div>
-    </div>
+<div class="container-fluid request-demo page-with-form customer-support-form">
+	<div class="container request-demo__container">
+		<?php
+		get_template_part( 'template-parts/template-heading' );
+
+		$hubspot_portal_id = get_field( 'portal_id', 'options' );
+		$hubspot_form_id   = get_field( 'form_guid', get_the_ID() );
+
+		if ( ! empty( $hubspot_portal_id ) && ! empty( $hubspot_form_id ) ) :
+			?>
+			<script charset="utf-8" type="text/javascript" src="//js.hsforms.net/forms/embed/v2.js"></script>
+			<script>
+				hbspt.forms.create({
+					region: "na1",
+					portalId: "<?= esc_html( $hubspot_portal_id ) ?>",
+					formId: "<?= esc_html( $hubspot_form_id ) ?>"
+				});
+			</script>
+		<?php endif; ?>
+	</div>
+</div>
+
 
 <?php
 get_footer();
