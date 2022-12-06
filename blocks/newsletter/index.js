5928d76b52ea0b4ab6fb9b76b22c2a8bd8012a96
matashaEs <natali.babinska@ukr.net>
2022-12-06 14:41:46 +0100

    start

--- blocks/newsletter/index.js
+++
@@ -1,1 +0,0 @@
-import './newsletter.scss';
--- index.php
+++ index.php
@@ -1,6 +1,33 @@
 <?php
-get_header();
 
-the_content();
+?>
 
-get_footer();
+<div>Input number and names (2 - 20)</div>
+<form method="post">
+    <input type="text" name="number">
+    <input type="submit" name="setNumber"
+           value="Go">
+</form>
+<?php
+
+if ( isset( $_POST['setNumber'] ) ) { ?>
+    <h3>Name of Criteria</h3>
+    <form method="post">
+		<?php for ( $i = 0; $i < $_POST['number']; $i ++ ) {
+			echo $i + 1 ?> <input type="text" name="criteria<?= $i ?>"><br>
+		<?php } ?>
+        <input type="submit" name="setCriteria"
+               value="Ok">
+    </form>
+<?php }
+
+if ( isset( $_POST['setCriteria'] ) ) { ?>
+    <h3>List</h3>
+	<?php
+     for ( $i = 0; $i < $_POST['number']; $i ++ ) {
+			echo $_POST['setCriteria' . $i];
+     }
+} ?>
+
+
+
